58d4721ef07afc465f92aec331face2a47f16f17
ren.meta@gmail.com <ren.meta@gmail.com>
2020-02-12 15:37:15 -0500

    added lastblock endpoint, working, removed proof of work from server

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,29 +5,37 @@ import sys
 import json
 
 
-def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+ def proof_of_work(self, block):
+        """
+        Simple Proof of Work Algorithm
+        Stringify the block and look for a proof.
+        Loop through possibilities, checking each one against `valid_proof`
+        in an effort to find a number that is a valid proof
+        :return: A valid proof for the provided block
+        """
+        block_string = json.dumps(block, sort_keys=True)
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
 
+        return proof
 
-def valid_proof(block_string, proof):
-    """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
-    leading zeroes?  Return true if the proof is valid
-    :param block_string: <string> The stringified block to use to
-    check in combination with `proof`
-    :param proof: <int?> The value that when combined with the
-    stringified previous block results in a hash that has the
-    correct number of leading zeroes.
-    :return: True if the resulting hash is a valid proof, False otherwise
-    """
-    pass
+    @staticmethod
+    def valid_proof(block_string, proof):
+        """
+        Validates the Proof:  Does hash(block_string, proof) contain 3
+        leading zeroes?  Return true if the proof is valid
+        :param block_string: <string> The stringified block to use to
+        check in combination with `proof`
+        :param proof: <int?> The value that when combined with the
+        stringified previous block results in a hash that has the
+        correct number of leading zeroes.
+        :return: True if the resulting hash is a valid proof, False otherwise
+        """
+        guess = f'{block_string}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:3] == "000"
 
 
 if __name__ == '__main__':
